c2af8cac7cffa436201cb317fc1dd7c90582cb45
ABE1112 <Abraham.Elie@lc.cuny.edu>
2019-11-19 10:45:20 -0500

    content commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget justo sed ante tincidunt faucibus in nec nulla. Nunc pharetra cursus felis, eu consectetur libero semper at. Donec vel placerat eros. Proin feugiat tempor orci, eget accumsan augue consequat id. Nullam non aliquam metus. Maecenas dapibus feugiat metus sit amet iaculis. Vestibulum cursus ipsum quis erat varius efficitur. Donec nunc lacus, sagittis et tempor at, ultrices at nisl. Cras elementum lectus et semper ornare. Phasellus cursus, sapien id vestibulum gravida, massa nibh auctor orci, nec cursus libero urna in orci. Pellentesque aliquet
+    sapien a lacus pulvinar convallis. Nunc at ante vel mauris placerat lacinia. Duis nibh ipsum, rutrum non eros vel, viverra
+    pretium ante.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sagittis risus vitae est pulvinar pellentesque.
+    Nam facilisis tempor ex, eget luctus odio aliquam ut. Maecenas nec enim sed dolor consectetur ultricies. Donec semper in risus
+    eget ornare. Sed gravida leo nec mauris auctor venenatis. Donec pellentesque purus tortor, id fermentum leo vehicula ac.
+    Sed in risus leo. Suspendisse porta imperdiet lorem, quis rutrum mauris auctor nec. Etiam consequat luctus mi, at euismod diam
+    fermentum quis. Aliquam egestas tellus tortor, vitae pharetra dui venenatis non. Cras pellentesque aliquam tellus ut dignissim.
+    Pellentesque feugiat eu nunc sed elementum. Etiam id semper lorem, eget volutpat nibh. Nunc fermentum auctor nisl ut rutrum.
+    Integer ac fermentum velit, non vulputate magna. Aenean pretium est ac nisl accumsan efficitur.
+    Maecenas finibus lacinia lorem, nec consectetur nibh interdum et. Pellentesque habitant morbi tristique senectus et
+    netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Cras in ex et ligula finibus tristique.
+    Sed varius vehicula sapien, et fringilla ante sagittis at. Donec mattis lacus tortor, sit amet semper ex vulputate nec.
+    Sed mattis libero id urna facilisis, efficitur commodo lorem sodales. Fusce mollis mi et leo sodales, ac auctor
+    sapien accumsan. Praesent ac efficitur nibh. Proin in dictum purus. Morbi vel blandit nisi.</p>
+  </div>
+)
---
+++ src/pages/Articles.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import events from 'events'
+import ajax from '../ajax'
+
+
+// Create an emitter object so that we can do pub/sub
+const emitter = new events.EventEmitter();
+
+const App = () => (
+ <center><div>
+  <p></p>
+    <NodeForm />
+    <NodeList />
+  </div></center>
+)
+
+// component to show a list of nodes
+class NodeList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      nodes: []
+    }
+    this.refresh = this.refresh.bind(this)
+  }
+
+  componentWillMount() {
+    emitter.addListener('NODE_UPDATED', this.refresh)
+  }
+
+  componentWillUnmount() {
+    emitter.addListener('NODE_UPDATED', this.refresh)
+  }
+
+  async componentDidMount() {
+    await this.refresh()
+  }
+
+  async refresh() {
+    // AJAX fetch server/node/rest?_format=json and setState with the response data
+    try {
+      const axios = await ajax() // wait for an initialized axios object
+      const response = await axios.get('/node/rest') // wait for the POST AJAX request to complete
+      if (response.data) {
+        // setState will trigger repaint
+        this.setState({ nodes: response.data })
+      }
+      } catch (e) {
+      alert(e)
+    }
+  }
+
+  render() {
+    const deleteNode = async (nid) => {
+      try {
+        const axios = await ajax() // wait for an initialized axios object
+        const response = await axios.delete(`/node/${nid}`) // wait for the DELETE AJAX request to complete
+        console.log('Node deleted', response)
+        emitter.emit('NODE_UPDATED')
+      } catch (e) {
+        alert(e)
+      }
+    }
+
+    return (
+    <table class="table-fill" style={{width: "100%", border: "1px solid black"}}>
+        <caption>Articles promoted to front page</caption>
+        <thead style={{border: "1px solid black"}}>
+          <tr>
+            <td>NID</td>
+            <td>Title</td>
+            <td>Delete</td>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.nodes.map((node, index) => {
+            // iterate over the nodes array and map them to "li" elements
+            return (
+              <tr key={index}>
+                <td>{node.nid}</td>
+                <td><a href={node.path} target="_blank">{node.title}</a></td>
+                <td><button onClick={e => deleteNode(node.nid)}>x</button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+const NodeForm = () => {
+  const data = {}
+  // note the 'async' keyword, it allows us to call 'await' later
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    var node = {
+      type: [{
+        target_id: 'article',
+        target_type: 'node_type',
+      }],
+      title: [{
+        value: data.title,
+      }],
+      body: [{
+        value: data.body,
+        format: 'plain_text',
+      }],
+    };
+    try {
+      const axios = await ajax() // wait for an initialized axios object
+      const response = await axios.post('/node', node) // wait for the POST AJAX request to complete
+      console.log('Node created: ', response)
+      emitter.emit('NODE_UPDATED')
+    } catch (e) {
+      alert(e)
+    }
+  }
+  const handleChange = (e, propName) => {
+    data[propName] = e.target.value
+  }
+
+  return (
+    <div style={{width: "100%"}}>
+      <h4>Create Node</h4>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <br />
+        <input style={{width: "40%"}} type="text" onChange={e => handleChange(e, 'title')}></input>
+        <br />
+        <label>Body</label>
+        <br />
+        <textarea style={{width: "40%", padding: "50px", height: "100px"}} onChange={e => handleChange(e, 'body')}></textarea>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default NodeList
